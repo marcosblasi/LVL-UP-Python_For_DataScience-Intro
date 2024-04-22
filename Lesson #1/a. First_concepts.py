18dcbb8035c7bafba0129951a6a8fd105bccc86c
marcosblasi <46039006+marcosblasi@users.noreply.github.com>
2024-04-22 11:31:10 -0300

    Create a. First_concepts.py

---
+++ Lesson #1/a. First_concepts.py
@@ -0,0 +1,17 @@
+# Define 3 variables that represent products of an e-commerce site and the prices of them.
+# Print a message of the selected product and the discount
+
+# mi_variable = 5
+# print(mi_variable)
+# print(f"Mi variable es {mi_variable}")
+
+prod_1 = "Adidas Boots"
+prod_2 = "Adidas Sweatshirt"
+prod_3 = "Adidas Ball"
+
+price_1 = 100000
+price_2 = 80000
+price_3 = 50000
+
+print(f"If you buy {prod_1} which value is {price_1} and {prod_2} which value is {price_2}, you get a {prod_3}, getting a {price_3} bonus")
+
