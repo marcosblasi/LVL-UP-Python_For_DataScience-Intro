8aba7c5a21eb99edc1e9712069a2da7cee431743
Marcos Blasi <46039006+marcosblasi@users.noreply.github.com>
2024-04-27 12:10:07 -0300

    Add files via upload

---
+++ Lesson #2/practice_exercise.py
@@ -0,0 +1,15 @@
+# Practice exercise
+
+
+
+product_list = ['Moon burguer', 'Super hot dog', 'Extra cheese chicken nuggets', 'Empanadas']
+new_supplier_list = ['Super double hot dog','Individual pizza']
+
+new_product_list = product_list + new_supplier_list
+
+new_product_list.remove('Empanadas')
+
+print(new_product_list)
+print(type(new_product_list))
+
+
