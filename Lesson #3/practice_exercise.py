ae29a0b3493d2b61c6cbb31dbaca1e483f669340
Marcos Blasi <46039006+marcosblasi@users.noreply.github.com>
2024-05-07 16:45:51 -0300

    Update practice_exercise.py

--- Lesson #3/practice_exercise.py
+++ Lesson #3/practice_exercise.py
@@ -1,1 +1,16 @@
+# Define a list of integer and float and obtain a total sum of elements in the list, in a new variable
 
+numbers = [2, 3.14, 45, 30, 446, 55, 43]
+
+total = 0
+total_iterations = 0
+
+for number in numbers:
+
+    total += number
+    total_iterations += 1
+
+
+print("The total sum of elements in list is = ", total)
+print("The total number of iterations is = ", total_iterations)
+print("The total number of iterations is = ", len(numbers))
